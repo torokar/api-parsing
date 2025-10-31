0d24251af6a1e4fe23802fe150eead9bffc73b0d
torokar <andrey.onkhonov95@bk.ru>
2025-10-31 13:41:59 +0800

    update

---
+++ recepts/recepts.py
@@ -0,0 +1,7 @@
+#https://www.russianfood.com/recipes/bytype/?fid=3#rcp_list) - записать каждый рецепт в отдельный файл
+
+from modules.http_codes import connection_website
+
+website_recepts = "https://www.russianfood.com/recipes/bytype/?fid=3#rcp_list) - записать каждый рецепт в отдельный файл"
+
+
